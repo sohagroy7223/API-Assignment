6d9378c9fcc8f9ddd3283675b0738fc9e6e86160
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-12 23:33:20 +0600

    addad smooth scroll but it is dont working

---
+++ sceipt/smoothScroll.js
@@ -0,0 +1,8 @@
+document.getElementById("FAQ-btn").addEventListener("click", () => {
+  const vocabularySec = document.getElementById("vocabulary");
+  vocabularySec.scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("learn-btn").addEventListener("click", () => {
+  const learnSec = document.getElementById("learn");
+  learnSec.scrollIntoView({ behavior: "smooth" });
+});
